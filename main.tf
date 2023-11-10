022fefb7ca9ce105a324b4ac858670a06efce9b9
Franco Navarro <francotel@Francos-MBP.lan>
2023-11-10 12:44:25 -0600

    first commit v28

--- main.tf
+++ main.tf
@@ -79,24 +79,21 @@ resource "azurerm_linux_virtual_machine" "main" {
 }
 
 resource "azurerm_mssql_server" "mssql" {
-  name                         = "demo-sqlserver"
+  name                         = "demo-sqlserver-2024"
   location                     = azurerm_resource_group.rg.location
   resource_group_name          = azurerm_resource_group.rg.name
   version                      = "12.0"
-  administrator_login          = "administrator"
+  administrator_login          = "4dm1n"
   administrator_login_password = "Qwerty123"
 }
 
 resource "azurerm_mssql_database" "dbsql" {
-  name           = "demo-dbs"
+  name           = "demo-dbs-2024"
   server_id      = azurerm_mssql_server.mssql.id
   collation      = "SQL_Latin1_General_CP1_CI_AS"
   license_type   = "LicenseIncluded"
-  max_size_gb    = 4
-  read_scale     = true
-  sku_name       = "S0"
-  zone_redundant = true
-
+  max_size_gb    = 10
+  sku_name       = "S2"
   tags = {
     environment = "Terraform Demo"
   }
